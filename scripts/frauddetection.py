556fb1fa666ba2a9c16544126bb77f08a2841309
fr17649 <franck.benault@worldline.com>
2025-01-07 23:22:34 +0100

    [DATA] visualization pie digram

--- scripts/frauddetection.py
+++ scripts/frauddetection.py
@@ -1,5 +1,13 @@
-#dummmy function
+import warnings
+warnings.filterwarnings('ignore')
 
-def foo():
-	return "foo"
+import pandas as pd 
+from datetime import datetime
+
+## import data 
+def read_file(inputFileName):
+    dateparse = lambda x: datetime.strptime(x, '%Y/%m/%d %H:%M:%S')
+    dfTrx = pd.read_csv(inputFileName, sep=";", parse_dates=['trx_date_time'], date_parser=dateparse)
+    
+    return dfTrx
 
