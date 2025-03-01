d38f92ca9eae89011f7555de8a26f13eee1831cc
fr17649 <franck.benault@worldline.com>
2025-03-01 22:09:24 +0100

    [FILTERING] add mcc 5542

--- scripts/datafilter.py
+++ scripts/datafilter.py
@@ -8,8 +8,13 @@ def reversalFiltering(dfTrx):
     dfRemained=dfTrx[(dfTrx['trx_reversal']!='PARTIAL REVERSAL')]
     return dfRemained
 
+def mccFiltering(dfTrx):
+    dfRemained=dfTrx[(dfTrx['term_mcc']!=5542)]
+    return dfRemained
+
 def fullFiltering(dfTrx,maxAmount=5_000):
     dfTrx=amountFiltering(dfTrx,maxAmount)
     dfTrx=reversalFiltering(dfTrx)
+    dfTrx=mccFiltering(dfTrx)
     return dfTrx
     
